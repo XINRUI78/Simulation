f7d0aead149e7d362e74757af59497f141802869
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-08-05 21:55:25 +0100

    Update generate_ss.R

--- tool/generate_ss.R
+++ tool/generate_ss.R
@@ -3,5 +3,5 @@ generate_ss <- function(n, n.para, beta0, beta){
   x <- mvtnorm::rmvnorm(n, mean = rep(0, n.para), sigma = diag(n.para))
   eta <- rep(beta0, n) + x%*%beta
   p <- 1/(1+exp(-eta))
-  y <- stats::rbinom(n, 1, p)
+  y <- rbinom(n, 1, p)
   data <- data.frame(y,x)}
