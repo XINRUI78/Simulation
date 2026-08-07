12ba643a461a5ca744903558a59d3ede83f2442c
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-08-07 12:32:54 +0100

    Update run_s3.R

--- tool/run_s3.R
+++ tool/run_s3.R
@@ -35,7 +35,7 @@ opt_beta_s3 <- function(n.para, n.true, prev, c, weights) {
   sigma[true.idx, noise.idx] <- 0.5
   sigma[noise.idx, true.idx] <- 0.5
   sigma[true.idx, true.idx] <- 0.5
-  sigma[noise.idx, noise.idx] <- 0
+  sigma[noise.idx, noise.idx] <- 0.5
   diag(sigma) <- 1
   # Generate data
   x <- rmvnorm(n, mean = rep(0, n.para), sigma = sigma)
