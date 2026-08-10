98222c88ddb24d162ab1a19bc4cd1109c747eb6a
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-08-10 15:16:02 +0100

    Update generate_ss_s3.R

--- tool/generate_ss_s3.R
+++ tool/generate_ss_s3.R
@@ -4,9 +4,9 @@ generate_ss_s3 <- function(n, n.para, n.true, beta0, beta, rho = 0.5){
   sigma <- diag(n.para)
   true.idx <- seq_len(n.true)
   noise.idx <- (n.true + 1):n.para
-  sigma[noise.idx, noise.idx] <- 0
-  sigma[true.idx, noise.idx] <- 0.2
-  sigma[noise.idx, true.idx] <- 0.2
+  sigma[noise.idx, noise.idx] <- 0.5
+  sigma[true.idx, noise.idx] <- 0.5
+  sigma[noise.idx, true.idx] <- 0.5
   sigma[true.idx, true.idx] <- rho
   
   # Variances must equal one
