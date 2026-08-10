78786167dfab4a5337bb8c96909beac87ee423c4
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-08-10 11:50:52 +0100

    Update test.R

--- test.R
+++ test.R
@@ -16,6 +16,15 @@ library(doParallel)
 library(foreach)
 library(RcppNumerical)
 library(brglm2)
-
+ # Generate a large dataset (200,000) using function generate_ss
+  # Check prevalence and C-statistic; 
+  n <- 200000; 
+  data <- generate_ss_s3(n, n.para, n.true, beta0, beta)
+  prev_ <- round(mean(data[,1]),2)
+  X <- as.matrix(data[,-1])
+  eta <- rep(beta0, n) + X%*%beta
+  p <- 1/(1+exp(-eta))
+  cstat <- pROC::roc(response = as.numeric(data[,1]), predictor = as.vector(p), levels = c(0, 1), direction = "<")
+  auc_ <- round(as.vector(cstat$auc),2)
 test <- perform_s3 (2, ndev, n.para, n.true, beta0, beta, nval, prev_, auc_)
 saveRDS(test,"result.rds")
