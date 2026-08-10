8b747f4fdcec792a8493a2c23d9289221f229ee7
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-08-10 11:16:56 +0100

    Add files via upload

---
+++ test.R
@@ -0,0 +1,21 @@
+# Source required scripts
+source("tool/generate_ss_s3.R")
+source("tool/perform_s3.R")
+source("tool/measures.R")
+source("tool/run_s3.R")
+source("method/back_logit.R")
+source("method/backward_pvalue.R")
+source("method/berank.R")
+source("method/lasso_exact.R")
+source("method/mod_penal_ave_foreach.R")
+source("method/stepwise_pvalue.R")
+source("method/unilogit.R")
+source("method/unirank.R")
+
+library(doParallel)
+library(foreach)
+library(RcppNumerical)
+library(brglm2)
+
+test <- perform_s3 (2, ndev, n.para, n.true, beta0, beta, nval)
+saveRDS(test,"result.rds")
