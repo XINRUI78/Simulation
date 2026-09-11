7ba8c0a87a185c38c4337d09b329aa1c0f1e9daa
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-09-11 14:46:56 +0100

    Rename function from perform_s3 to perform_s2

--- tool/perform_s2.R
+++ tool/perform_s2.R
@@ -1,18 +1,18 @@
 library(doParallel)
 library(foreach)
 
-perform_s3 <- function(i, ndev, n.para, n.true, beta0, beta, nval, prev, auc){
+perform_s2 <- function(i, ndev, n.para, n.true, beta0, beta, nval, prev, auc){
  
 
                                                                                                      set.seed(i)
                                                                                                      
                                                                                                      # generate a development dataset of size ndev=2000
-                                                                                                     data.dev <- generate_ss_s3(ndev, n.para, n.true, beta0, beta)
+                                                                                                     data.dev <- generate_ss_s2(ndev, n.para, n.true, beta0, beta)
                                                                                                      x <- data.dev[,-1]
                                                                                                      y <- data.dev[,1]
                                                                                                      
                                                                                                      # generate a validation dataset of size nval
-                                                                                                     data.val <- generate_ss_s3(nval, n.para, n.true, beta0, beta)
+                                                                                                     data.val <- generate_ss_s2(nval, n.para, n.true, beta0, beta)
                                                                                                      xval <- data.val[,-1]
                                                                                                      yval <- data.val[,1]
                                                                                                      
