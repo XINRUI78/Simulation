f004d2113307d061001076af168abb5c734a885b
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-09-13 20:51:26 +0100

    Update exported functions in run_simulation.R

--- run/run_simulation.R
+++ run/run_simulation.R
@@ -44,7 +44,7 @@ run_simulation <- function(percentage, output_name, ndev, n.restrict, n.para = 3
     i = seq_len(nrep),
     .combine = rbind,
     .packages = c("mvtnorm", "pROC", "glmnet"),
-    .export = c("berank", "lasso_exact", "unirank", "perform"),
+    .export = c("backward_pvalue", "mod_penal_ave_foreach", "berank", "lasso_exact", "unirank", "unilogit", "perform", "generate_ss", "measures","opt_beta"),
     .errorhandling = "pass"
   ) %dopar% {
 
