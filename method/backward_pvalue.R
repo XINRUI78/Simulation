d066011f23f2781e25e22ea21a62bc98eda85dbc
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-09-13 21:05:28 +0100

    Update indicator calculation to use all predictors

--- method/backward_pvalue.R
+++ method/backward_pvalue.R
@@ -30,10 +30,11 @@ backward_pvalue <- function(data, response, p_threshold = 0.05) {
     }
   }
   # Get the final predictors
+  all_predictors <- setdiff(names(data), response)
   back_predictors <- names(coef(model))[-1] # Exclude the intercept
   
   # Create the indicator row for variable retention
-  indicator <- ifelse(predictors %in% back_predictors, 1, 0)
+  indicator <- ifelse(all_predictors %in% back_predictors, 1, 0)
   
   # Return the final model and indicator row
   return(list(
