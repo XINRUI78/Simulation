3272aa2c115734758093e84c090e1dba2908791c
Xinrui Su <135606485+XINRUI78@users.noreply.github.com>
2026-09-22 23:12:03 +0100

    Uncomment and update simulation parameters

--- tool/run_s2.R
+++ tool/run_s2.R
@@ -1,10 +1,8 @@
 # Set simulation parameters
 n.para <- 30
 n.true <- 15
-#prev <- 0.3
-#c <- 0.8
-#prev <- 0.1
-#c <- 0.7
+prev <- 0.3
+c <- 0.8
 nval <- 10000
 percentage <- c(0.1, 0.2, 0.2, 0.5)
 
@@ -22,8 +20,7 @@ weights <- c(rep(1, strong), rep(0.5, medium), rep(0.25, weak), rep(0, noise))
 
 #rss <- samplesizedev(outcome = "Binary", S = 0.9, phi = prev, c = c, p = n.para)
 #ndev <- rss$sim # recommended sample size
-#ndev0 <- 1505
-ndev0 <- 6184
+ndev0 <- 1505
 ndev1 <- round(ndev0/2) # half the recommended sample size
 ndev2 <- round(ndev0/4) # one-quarter recommended sample size
 ndev3 <- round(3*ndev0/4) # 3/4 recommended sample size
